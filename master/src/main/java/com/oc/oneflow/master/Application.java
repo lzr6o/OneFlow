949adad09e03c7c688affe28b88eb5d7e45decd0
李zr <zr6oli@gmail.com>
2022-03-03 20:51:15 -0500

    push

--- master/src/main/java/com/oc/oneflow/master/Application.java
+++ master/src/main/java/com/oc/oneflow/master/Application.java
@@ -1,22 +1,21 @@
 package com.oc.oneflow.master;
 
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
+import com.oc.oneflow.common.utils.ConfigUtil;
 import com.oc.oneflow.model.ConfigVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 @SpringBootApplication
 public class Application {
-    @Value("${jsonConfigPath}")
-    private String jsonConfigPath;
+
+    @Autowired
+    private ConfigUtil configUtil;
 
     private static final Logger appLogger = LoggerFactory.getLogger(Application.class);
 
@@ -26,9 +25,7 @@ public class Application {
 
     @PostConstruct
     public void run() throws FileNotFoundException {
-        Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new FileReader(jsonConfigPath));
-        ConfigVO configVO = gson.fromJson(reader, ConfigVO.class);
+        ConfigVO configVO = configUtil.getConfigVO();
         System.out.println(configVO.getProjectId());
         System.out.println(configVO.getProjectName());
     }
